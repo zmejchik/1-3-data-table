6e6a12ee7bf8dda3b6009b74e725ac38ded0214f
zmejchik <zmejchik@gmail.com>
2023-11-21 18:46:57 +0200

    add function sort after click

--- js/script.js
+++ js/script.js
@@ -9,7 +9,11 @@ function DataTable(config, data) {
 function titleRow(config) {
   let result = "<thead> <tr> <th>№</th>";
   for (let element of config.columns) {
-    result = result + "<th>" + element.title + "</th>";
+    result =
+      result +
+      `<th onclick="sortingTable('${element.value}')">` +
+      element.title +
+      "</th>";
   }
   result = result + "</tr> </thead>";
   return result;
@@ -43,18 +47,52 @@ const config1 = {
 };
 
 const users = [
-  { id: 30050, name: 'Вася', surname: 'Петров', age: 12 },
-  { id: 30051, name: 'Вася', surname: 'Васечкін', age: 15 },
-  { id: 30050, name: 'Вася', surname: 'Петров', age: 18 },
-  { id: 30051, name: 'Тарас', surname: 'Шевченко', age: 35 },
-  { id: 30050, name: 'Толя', surname: 'Васечкін', age: 47 },
-  { id: 30051, name: 'Ліна', surname: 'Костенко', age: 31 },
-  { id: 30051, name: 'Лариса', surname: 'Косач', age: 25 },
-  { id: 30051, name: 'Юля', surname: 'Мойсик', age: 30 },
+  { id: 30050, name: "Вася", surname: "Петров", age: 12 },
+  { id: 30051, name: "Вася", surname: "Васечкін", age: 15 },
+  { id: 30050, name: "Вася", surname: "Петров", age: 18 },
+  { id: 30051, name: "Тарас", surname: "Шевченко", age: 35 },
+  { id: 30050, name: "Толя", surname: "Васечкін", age: 47 },
+  { id: 30051, name: "Ліна", surname: "Костенко", age: 31 },
+  { id: 30051, name: "Лариса", surname: "Косач", age: 25 },
+  { id: 30051, name: "Юля", surname: "Мойсик", age: 30 },
 ];
 
 DataTable(config1, users);
+/**Function for sortin column */
+let sortOrder = 1;
+function sortingTable(nameColumn) {
+  sortOrder = sortOrder * -1;
+  result = users.sort((a, b) => {
+    let A = a[nameColumn];
+    let B = b[nameColumn];
 
+    if (!isNaN(parseFloat(A)) && !isNaN(parseFloat(B))) {
+      return sortOrder * (parseFloat(A) - parseFloat(B));
+    } else {
+      if (A > B) return 1 * sortOrder;
+      if (A < B) return -1 * sortOrder;
+      return 0;
+    }
+  });
+  DataTable(config1, result);
+  return result;
+}
+/* if (nameColumn === 'id') return a.id - b.id; */
+/* if (nameColumn === 'name') {
+      if (a.name > b.name) return 1;
+      if (a.name < b.name) return -1;
+      return 0;
+    }
+    if (nameColumn === 'surname') {
+      if (a.surname > b.surname) return 1;
+      if (a.surname < b.surname) return -1;
+      return 0;
+    }
+    if (nameColumn === 'age') return a.age - b.age; 
+    return true;
+  });
+  DataTable(config1, result);
+}*/
 /* Tabulator */
 //create Tabulator on DOM element with id "example-table"
 var table = new Tabulator("#example-table", {
